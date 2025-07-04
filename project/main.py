632e2cf1bf26adbd1b5024abeb1805a72d0b37a3
osmar-dev <oaba.dev@gmail.com>
2025-07-03 23:56:18 -0400

    dockerization of the project

---
+++ project/main.py
@@ -0,0 +1,9 @@
+# project/test_python.py
+
+print("Python is working correctly inside the Docker container!")
+
+# You can add more complex tests here if needed, e.g.:
+# import sys
+# print(f"Python version: {sys.version}")
+# import os
+# print(f"Current working directory: {os.getcwd()}")
